06f4b839c407a675152d9e1f37d7865feef30c3a
Syed Taha <143250416+syedtaha22@users.noreply.github.com>
2025-05-04 00:19:35 +0500

    update main for streamlined visualisation

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import numpy as np
 import gymnasium as gym
 from taxi_q_learner import TaxiQLearner
 
+# dir_name = 'hyperparameter_tuning'
+# dir_name = 'baseline'
+dir_name = 'modified_environment'
+
 if __name__ == "__main__":
     agent = TaxiQLearner()
-    agent.train('training_log.csv')
-    agent.save_q_table('q_table.csv')
+    agent.load_q_table(f'{dir_name}/q_table.csv', verbose=True)
 
-    agent.run(num_episodes=1000, render_mode='human', verbose=1)
+    try:
+        env = gym.make('Taxi-v3', render_mode='human')
+        print("Training complete. Press Ctrl+C to exit.")
+        runs = 0 # For calculating average reward over runs
+        total_reward = 0 # For calculating average reward over runs
 
-    # try:
-    #     env = gym.make('Taxi-v3', render_mode='human')
-    #     print("Training complete. Press Ctrl+C to exit.")
-    #     runs = 0 # For calculating average reward over runs
-    #     total_reward = 0 # For calculating average reward over runs
-
-    #     while True:
-    #         state, _ = env.reset()
-    #         done = False
-    #         runs += 1
-    #         while not done:
-    #             action = np.argmax(agent.Q[state])
-    #             state, reward, terminated, truncated, _ = env.step(action)
-    #             done = terminated or truncated
-    #             total_reward += reward
-    #         print(f"Run {runs}: Average Reward: {total_reward/runs:.2f}")
-    # except KeyboardInterrupt:
-    #     print("\nDemo terminated.")
+        while True:
+            state, _ = env.reset()
+            done = False
+            runs += 1
+            while not done:
+                action = np.argmax(agent.Q[state])
+                state, reward, terminated, truncated, _ = env.step(action)
+                done = terminated or truncated
+                total_reward += reward
+            print(f"Run {runs}: Average Reward: {total_reward/runs:.2f}")
+    except KeyboardInterrupt:
+        print("\nDemo terminated.")
