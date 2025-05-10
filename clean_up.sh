6347c98ef941d546d1e96105b3fdb248a27b1fdf
Syed Taha <143250416+syedtaha22@users.noreply.github.com>
2025-05-10 16:21:36 +0500

    update cleanup script to include modified_environments directory

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-# Clean up baseline/ and tuning/ directories
+# Clean up baseline/ and hyperparameter_tuning/ directories
 
 echo "Cleaning up baseline/..."
 rm -v baseline/*.csv 2>/dev/null
@@ -10,5 +10,9 @@ echo "Cleaning up hyperparameter_tuning/..."
 # rm -v hyperparameter_tuning/*.csv 2>/dev/null
 rm -v hyperparameter_tuning/training_logs/*.csv 2>/dev/null
 rm -v hyperparameter_tuning/q_tables/*.csv 2>/dev/null
+
+# Remove all .csv files in the modified_environments/ directory
+echo "Cleaning up modified_environments/..."
+rm -v modified_environments/*.csv 2>/dev/null
 
 echo "Cleanup complete."
